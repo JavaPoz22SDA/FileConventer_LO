08cbf76d8fc71e1aacaf31785b8ab8cb62d08bb5
Lukasz <lukasz.ogan@gmail.com>
2020-05-16 12:07:31 +0200

    get json by key

--- src/main/java/pl/sda/fileconvneter/JSONReader.java
+++ src/main/java/pl/sda/fileconvneter/JSONReader.java
@@ -1,5 +1,8 @@
 package pl.sda.fileconvneter;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,6 +19,19 @@ public class JSONReader implements Reader{
             path = Paths.get(inputFilePath);
             byte[] bytes = Files.readAllBytes(path);
             System.out.println(bytes);
+            String content = new String(bytes);
+
+            JSONArray jsonArray = new JSONArray(content);
+
+            for(int i=0; i<jsonArray.length(); i++){
+                Object rawObject = jsonArray.get(i);
+                JSONObject record = new JSONObject(rawObject.toString());
+                System.out.println(jsonArray.get(i));
+                System.out.println("Miasto: " + record.get("city"));
+                for(String key: record.keySet()){
+                    System.out.println(record.get(key));
+                }
+            }
 
         }catch (IOException ex){
             ex.printStackTrace();
